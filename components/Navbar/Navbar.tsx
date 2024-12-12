3abf5d513afcc56db98afbba1741f184cc7c1263
sasasanono <ukakau@gmail.com>
2024-12-12 15:48:35 +0900

    fix navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { MagnifyingGlass } from 'phosphor-react'
 import React from 'react'
 
 const Navbar = () => {
@@ -6,18 +7,16 @@ const Navbar = () => {
     <nav className='container mx-auto lg:px-2 px-5 lg:w-2/5'>
         <div className='container flex items-center justify-between mx-auto'>
             <Link href="/" className='text-2xl font-medium'>
-                Blog
+                Horizon-TechShelf
             </Link>
             <div>
                 <ul className='flex items-center text-sm py-4'>
                     <li>
-                        <Link href="/" className='block px-4 py-2 hover:text-sky-900 transition-all duration-300'>Home</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className='block px-4 py-2 hover:text-sky-900 transition-all duration-300'>Twitter</Link>
-                    </li>
-                    <li>
-                        <Link href="#" className='block px-4 py-2 hover:text-sky-900 transition-all duration-300'>Qiita</Link>
+                        <button className="p-2 bg-gray-100 hover:bg-gray-200 rounded-full shadow-md">
+                            <MagnifyingGlass size={24} weight="bold" className="text-gray-700">
+                                <Link href='/'></Link>
+                            </MagnifyingGlass>
+                        </button>
                     </li>
                 </ul>
             </div>
