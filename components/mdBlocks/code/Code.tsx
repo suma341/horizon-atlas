e4f1d5a8a35a17a50290c68865740acc0d2535d2
sasasanono <ukakau@gmail.com>
2025-01-09 17:53:31 +0900

    change layout of code

--- components/mdBlocks/code/Code.tsx
+++ components/mdBlocks/code/Code.tsx
@@ -32,12 +32,10 @@ export default function Code(props: Props) {
     return (
         <div className='mb-2 mt-4 relative w-11/12'>
             <div className='mb-0' style={{background: "rgb(245, 245, 245)"}}>
-                <p className='text-neutral-800 px-2 text-base py-1'>{language}</p>
+                <p className='text-neutral-400 px-2 text-base py-1'>{language}</p>
                 <button
                     onClick={handleCopy}
-                    className={`absolute top-0.5 right-2 px-2 py-1 text-sm rounded ${
-                        copied ? 'bg-neutral-600 text-white' : 'bg-gray-100 text-gray-800'
-                    }`}
+                    className={'absolute top-0.5 right-2 px-2 py-1 text-sm rounded bg-gray-100 text-gray-500 '}
                 >
                     {copied ? 'Copied' : 'Copy'}
                 </button>
