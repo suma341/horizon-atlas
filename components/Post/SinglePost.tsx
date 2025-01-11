c27e0d5e7a088d11dde90ed874e0b6f0b6730dcf
sasasanono <ukakau@gmail.com>
2025-01-11 14:40:33 +0900

    create tagStyle

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PostMetaData } from "@/types/postMetaData";
 import Link from 'next/link';
 import Image from 'next/image';
+import { tagStyle } from '@/styles/tag/tagStyle';
 
 type Props = {
     postData:PostMetaData;
@@ -16,14 +17,14 @@ const SinglePost = (props:Props) => {
             {isPagenationPage ? (
                 <section className='bg-slate-50 mb-4 mx-auto rounded-md p-3 shadow-2xl hover:shadow-none hover:translate-y-1 hover:bg-neutral-100 transition-all duration-300'>
                     <div className='flex w-auto h-9 my-1'>
-                        {icon!=='' && <Image src={icon} alt={""} width={20} height={20} className='relative w-8 h-auto m-0 mr-1 bottom-1' />}
+                        {icon!=='' && <Image src={icon} alt={title} width={20} height={20} className='relative w-8 h-auto m-0 mr-1 bottom-1' />}
                         <h2 className='text-xl font-medium mb-2'>
                             {title}
                         </h2>
                     </div>
                     <div className='flex flex-wrap'>
                         {tags.map((tag, i:number)=>(
-                            <span className='bg-neutral-400 text-white rounded-md px-1 pb-1/2 mr-2 mb-1 font-thin' key={i}>
+                            <span style={tagStyle} key={i}>
                                 {tag}
                             </span>
                         ))}
@@ -32,13 +33,13 @@ const SinglePost = (props:Props) => {
             ) : (
                 <section className='bg-slate-50 mb-4 mx-auto rounded-md p-3 shadow-2xl hover:shadow-none hover:translate-y-1 hover:bg-neutral-100 transition-all duration-300'>
                     <div className='flex w-auto h-9 my-1'>
-                    {icon!=='' && <Image src={icon} alt={""} width={20} height={20} className='w-9 h-auto m-0 mr-1 relative bottom-1' />}
+                    {icon!=='' && <Image src={icon} alt={title} width={20} height={20} className='w-9 h-auto m-0 mr-1 relative bottom-1' />}
                         <h2 className='text-xl font-medium mb-2 mr-1'>
                             {title}
                         </h2>
                     </div>
                     {tags.map((tag, i:number)=>(
-                        <span className='text-white bg-neutral-400 rounded-md px-1 pb-1 mr-2 font-thin' key={i}>
+                        <span style={tagStyle} key={i}>
                             {tag}
                         </span>
                     ))}
