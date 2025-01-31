2c4e403ef02952e2104c5367d9c59f74b8ef6187
sasasanono <ukakau@gmail.com>
2025-02-01 03:45:27 +0900

    ログイン機能復活

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,23 +2,23 @@ import React,{ ReactNode,  useEffect, useState } from 'react'
 import Head from 'next/head';
 import Footer from './Footer/Footer';
 import { useSession } from 'next-auth/react';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import Header from './Header/Header';
 import { pageNav } from '@/types/pageNav';
 import Navbar from './Navbar/navbar';
 import Sidebar from './Sidebar/Sidebar';
 
-// const PUBLIC_PAGES = ["/"]; // 認証不要なページ
+const PUBLIC_PAGES = ["/"]; // 認証不要なページ
 
 function AuthGuard({ children }: { children: React.ReactNode }) {
   const { data: session, status } = useSession(); // statusを直接取得
-  // const router = useRouter();
+  const router = useRouter();
 
-  // useEffect(() => {
-  //   if (status === "unauthenticated" && !PUBLIC_PAGES.includes(router.pathname)) {
-  //     router.push("/"); // 未ログインで公開ページ以外にアクセスした場合は初期ページへ
-  //   }
-  // }, [status, router]);
+  useEffect(() => {
+    if (status === "unauthenticated" && !PUBLIC_PAGES.includes(router.pathname)) {
+      router.push("/"); // 未ログインで公開ページ以外にアクセスした場合は初期ページへ
+    }
+  }, [status, router]);
 
   if (status === "loading") {
     return <p className='mx-auto my-0'>Loading...</p>; // ローディング中の表示
@@ -84,15 +84,15 @@ const Layout:React.FC<LayoutProps> = ({ children,headerProps,sideNavProps })=> {
         <Head>
           <title>Horizon Atlas</title>
         </Head>
-        <Sidebar openbar={openbar} setOpenbar={setOpenbar} allTags={headerProps.allTags} pageNav={sideNavProps} />
-        <div className='fixed top-0 z-50 w-full duration-500'  style={isVisible ? {transform: "translateY(0px)"} : {transform: "translateY(-65%)"}}>
-          <Header searchKeyWord={headerProps.searchKeyWord} setOpenbar={setOpenbar} />
-          <Navbar pageNavs={headerProps.pageNavs} />
-        </div>
         <SesstionProviderWraped>
-          {children}
+          <Sidebar openbar={openbar} setOpenbar={setOpenbar} allTags={headerProps.allTags} pageNav={sideNavProps} />
+          <div className='fixed top-0 z-50 w-full duration-500'  style={isVisible ? {transform: "translateY(0px)"} : {transform: "translateY(-65%)"}}>
+            <Header searchKeyWord={headerProps.searchKeyWord} setOpenbar={setOpenbar} />
+            <Navbar pageNavs={headerProps.pageNavs} />
+          </div>
+            {children}
+          <Footer />
         </SesstionProviderWraped>
-        <Footer />
     </div>
   );
 };
