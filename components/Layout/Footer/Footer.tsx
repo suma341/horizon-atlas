dab70b4dd678c22b25eec02f2ec6fb73facdc66a
sasasanono <ukakau@gmail.com>
2025-02-16 21:16:05 +0900

    footerから余計な要素を削除

--- components/Layout/Footer/Footer.tsx
+++ components/Layout/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Image from 'next/image';
 
 const Footer = () => {
   return (
@@ -20,14 +19,6 @@ const Footer = () => {
         </div>
         <div>
           <h4 className="font-bold text-lg mb-2">Follow Us</h4>
-          <div className="flex space-x-4">
-            <a href="https://twitter.com" aria-label="Twitter" className='mt-1'>
-              <Image src="/x-social-media-logo-icon.svg" alt="X-icon" width={10} height={10} className="w-8 h-8" />
-            </a>
-            <a href="https://facebook.com" aria-label="Facebook">
-              <Image src="/Instagram-Logo.wine.svg" alt="Instagram-icon" width={10} height={10} className="w-10 h-10" />
-            </a>
-          </div>
         </div>
       </div>
       <p className="text-center mt-4">©2024 RyukokuHorizon.</p>
