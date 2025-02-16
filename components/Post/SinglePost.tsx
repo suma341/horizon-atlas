28271d3352d413692b87bb37830ae215818f060c
sasasanono <ukakau@gmail.com>
2025-02-16 12:48:21 +0900

    アイコンダウンロードファイル参照

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -17,7 +17,7 @@ const SinglePost = (props:Props) => {
             {isPagenationPage ? (
                 <section className='bg-slate-50 mb-4 rounded-md p-3 shadow-2xl hover:shadow-none hover:translate-y-1 hover:bg-neutral-100 transition-all duration-300'>
                     <div className='flex w-auto h-9 my-1'>
-                        {icon!=='' && <Image src={icon} alt={title} width={20} height={20} className='relative w-8 h-auto m-0 mr-1 bottom-1' />}
+                        {icon!=='' && <Image src={`/horizon-atlas/notion_data/icon/${slug}.png`} alt={title} width={20} height={20} className='relative w-8 h-auto m-0 mr-1 bottom-1' />}
                         <h2 className='text-xl font-medium mb-2'>
                             {title}
                         </h2>
@@ -31,7 +31,7 @@ const SinglePost = (props:Props) => {
             ) : (
                 <section className='bg-slate-50 mb-4 mx-auto rounded-md p-3 shadow-2xl hover:shadow-none hover:translate-y-1 hover:bg-neutral-100 transition-all duration-300'>
                     <div className='flex w-auto h-9 my-1'>
-                    {icon!=='' && <Image src={icon} alt={title} width={20} height={20} className='w-9 h-auto m-0 mr-1 relative bottom-1' />}
+                    {icon!=='' && <Image src={`/horizon-atlas/notion_data/icon/${slug}.png`} alt={title} width={20} height={20} className='w-9 h-auto m-0 mr-1 relative bottom-1' />}
                         <h2 className='text-xl font-medium mb-2 mr-1'>
                             {title}
                         </h2>
