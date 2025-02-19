31ae9607a7a497f8c1289db2c0a086d5fcdf419a
sasasanono <ukakau@gmail.com>
2025-02-19 22:40:29 +0900

    output:exportに

--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,5 @@
 import { User } from "@auth0/auth0-react";
 
-type Guild = {
-    id: string;
-    name: string;
-    icon?: string;
-  };  
-
 declare interface CustomUser extends Omit<User, "profile"> {
-    profile: Guild[] | undefined;
+    profile: boolean | undefined;
   }
