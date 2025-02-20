f51ac77d43a16d94c84c110af856003bf5123cdb
sasasanono <ukakau@gmail.com>
2025-02-20 18:02:27 +0900

    カリキュラムタイトルに文字数制限付与

--- components/Post/SinglePost.tsx
+++ components/Post/SinglePost.tsx
@@ -17,7 +17,7 @@ const SinglePost = (props:Props) => {
                 <section className='mb-4 mx-5 rounded-md p-3 shadow-md hover:shadow-none hover:translate-y-1 hover:bg-neutral-50 transition-all duration-200 border'>
                     <div className='flex w-auto h-9 my-1'>
                         {icon!=='' && <Image src={`/horizon-atlas/notion_data/eachPage/${slug}/icon.png`} alt={title} width={30} height={30} className='relative w-8 h-auto m-0 mr-1 bottom-1' />}
-                        <h2 className='text-xl font-medium mb-2'>
+                        <h2 className='text-xl font-medium mb-2 line-clamp-1'>
                             {title}
                         </h2>
                     </div>
@@ -36,7 +36,7 @@ const SinglePost = (props:Props) => {
                 <section className='mb-4 mx-5 rounded-md p-3 shadow-md hover:shadow-none hover:translate-y-1 hover:bg-neutral-50 transition-all duration-200 border'>
                     <div className='flex w-auto h-9 my-1'>
                     {icon!=='' && <Image src={`/horizon-atlas/notion_data/eachPage/${slug}/icon.png`} alt={title} width={30} height={30} className='w-9 h-auto m-0 mr-1 relative bottom-1' />}
-                        <h2 className='text-xl font-medium mb-2 mr-1'>
+                        <h2 className='text-xl font-medium mb-2 mr-1 line-clamp-1'>
                             {title}
                         </h2>
                     </div>
