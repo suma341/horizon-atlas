9512cb7152031f7938a8f896ee80335d782b7474
sasasanono <ukakau@gmail.com>
2025-02-20 15:50:49 +0900

    サイドバーをスクロール可能に

--- components/Layout/Sidebar/Sidebar.tsx
+++ components/Layout/Sidebar/Sidebar.tsx
@@ -34,38 +34,42 @@ function Sidebar({openbar,setOpenbar,allTags,pageNav}:Props) {
                 onClick={()=>setOpenbar(false)}
                 className='bg-black opacity-45 fixed w-full top-0 block md:hidden' style={{height:`${getPageHeight()}px`,zIndex:"9998"}}>
                 </div>}
-            <div className='fixed w-7/12 top-0 right-0 block md:hidden bg-white duration-700'
-            style={openbar ? {transform:"translateX(0px)",height:`${getPageHeight()}px`,zIndex:"9999"}:{transform:"translateX(130%)", height:"100%"}}>
-                <ul>
-                    <li className='flex items-center justify-between mr-5'>
-                        <div></div>
-                        <p onClick={()=>setOpenbar(false)} className='cursor-pointer text-4xl text-neutral-500'>×</p>
-                    </li>
-                    <li className='flex items-center justify-between mr-5'>
-                        <div></div>
-                        <UserBlock />
-                    </li>
-                    <li className='px-5 mt-2'>
-                        <SearchField searchKeyWord={''} />
-                    </li>
-                    <li className='px-5 mt-2'>
-                        <Tags allTags={allTags} />
-                    </li>
-                    <li className='px-5 mt-2'>
-                        {pageNav!==undefined && <div className='p-1 border border-neutral-400 rounded'>
-                            <p className='text-base'>{pageNav.title}</p>
-                            {pageNav.childPages.map((nav,i)=>{
-                                return (
-                                    <div key={i} className="p-0.5 mt-1 cursor-pointer hover:bg-neutral-100">
-                                        <Link href={`/posts/post/${pageNav.slug}/${nav.id}`} className="my-1.5 text-sm text-neutral-500 underline truncat">
-                                            {nav.title}
-                                        </Link>
-                                    </div>)
-                            })}
-                        </div>}
-                    </li>
-                </ul>
-            </div>
+                <div className='fixed w-7/12 top-0 right-0 block md:hidden bg-white duration-700 h-screen'
+    style={openbar ? { transform: "translateX(0px)", zIndex: "9999" } : { transform: "translateX(130%)" }}>
+    <div className="h-full overflow-y-auto p-5">
+        <ul>
+            <li className='flex items-center justify-between mr-5'>
+                <div></div>
+                <p onClick={() => setOpenbar(false)} className='cursor-pointer text-4xl text-neutral-500'>×</p>
+            </li>
+            <li className='flex items-center justify-between mr-5'>
+                <div></div>
+                <UserBlock />
+            </li>
+            <li className='mt-2'>
+                <SearchField searchKeyWord={''} />
+            </li>
+            <li className='mt-2'>
+                <Tags allTags={allTags} />
+            </li>
+            <li className='mt-2'>
+                {pageNav !== undefined && (
+                    <div className='p-1 border border-neutral-400 rounded'>
+                        <p className='text-base'>{pageNav.title}</p>
+                        {pageNav.childPages.map((nav, i) => (
+                            <div key={i} className="p-0.5 mt-1 cursor-pointer hover:bg-neutral-100">
+                                <Link href={`/posts/post/${pageNav.slug}/${nav.id}`} className="my-1.5 text-sm text-neutral-500 underline truncate">
+                                    {nav.title}
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </li>
+        </ul>
+    </div>
+</div>
+
         </>
     )
 }
