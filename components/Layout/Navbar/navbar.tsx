dac2e32d91defa88db6613cf93a909d8c487a8aa
sasasanono <ukakau@gmail.com>
2025-02-23 09:35:39 +0900

    navbar幅調整

--- components/Layout/Navbar/navbar.tsx
+++ components/Layout/Navbar/navbar.tsx
@@ -22,14 +22,12 @@ function Navbar(props:Props) {
                         <p className='cursor-default text-neutral-500'>/</p>
                         <DetailNav pageNav={pageNavs.slice(1,-2)} />
                         <p className='cursor-default text-neutral-500'>/</p>
-                        <Link href={pageNavs[pageNavs.length - 2].id} className='px-2 hover:bg-neutral-200 text-neutral-500'>
-                            {pageNavs[pageNavs.length - 2].title.slice(0,8)}
-                            {pageNavs[pageNavs.length - 2].title.length>8 &&<span>...</span>}
+                        <Link href={pageNavs[pageNavs.length - 2].id} className='px-2 hover:bg-neutral-200 text-neutral-500 truncate max-w-[clamp(100px, 20vw, 250px)]'>
+                            {pageNavs[pageNavs.length - 2].title}
                         </Link>
                         <p className='cursor-default text-neutral-500'>/</p>
-                        <Link href={pageNavs[pageNavs.length - 1].id} className='px-2 hover:bg-neutral-200 text-neutral-500'>
-                            {pageNavs[pageNavs.length - 1].title.slice(0,8)}
-                            {pageNavs[pageNavs.length - 1].title.length>8 &&<span>...</span>}
+                        <Link href={pageNavs[pageNavs.length - 1].id} className='px-2 hover:bg-neutral-200 text-neutral-500 truncate max-w-[clamp(100px, 20vw, 250px)]'>
+                            {pageNavs[pageNavs.length - 1].title}
                         </Link>
                     </div>
                 </div>}
@@ -39,9 +37,8 @@ function Navbar(props:Props) {
                             {i===0 && <Link href={nav.id} className='pr-2 pt-0.5 pl-1 hover:bg-neutral-200 text-neutral-500'>
                                 <IoHomeOutline size={18} />
                             </Link>}
-                            {i!==0 && <Link href={nav.id} className='px-2 hover:bg-neutral-200 text-neutral-500'>
-                                {nav.title.slice(0,9)}
-                                {nav.title.length>9 &&<span>...</span>}
+                            {i!==0 && <Link href={nav.id} className='px-2 hover:bg-neutral-200 text-neutral-500 truncate max-w-[clamp(100px, 20vw, 250px)]'>
+                                {nav.title}
                             </Link>}
                             {i + 1 < pageNavs.length && <p className='cursor-default text-neutral-500'>/</p>}
                         </div>
