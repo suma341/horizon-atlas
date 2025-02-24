10a5bf7ffe70df45bb9ff7bf1553a08862b1b66e
sasasanono <ukakau@gmail.com>
2025-02-24 17:17:10 +0900

    supabase_urlをsecretに

--- lib/fetchRoleInfo.ts
+++ lib/fetchRoleInfo.ts
@@ -2,7 +2,8 @@ import { RoleData } from "@/types/role";
 
 export async function fetchRoleInfo(){
     const API_KEY = process.env.SUPABASE_ANON_KEY!;
-    const res = await fetch("https://cyqgvoqlgqqkppsbahkn.supabase.co/functions/v1/get_role", {
+    const SUPABASE_URL = process.env.SUPABASE_URL!;
+    const res = await fetch(`${SUPABASE_URL}/functions/v1/get_role`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
