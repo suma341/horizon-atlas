d2035715c147f1e5517dfb890542f9e278fd98fd
sasasanono <ukakau@gmail.com>
2025-02-25 12:08:52 +0900

    スクロールをsmoothに

--- components/mdBlocks/table_of_contents/table_of_contents.tsx
+++ components/mdBlocks/table_of_contents/table_of_contents.tsx
@@ -1,5 +1,4 @@
 import { findHeadingBlock } from '@/lib/findHeadingBlock';
-import Link from 'next/link';
 import { MdBlock } from 'notion-to-md/build/types';
 import React, { useEffect, useState } from 'react'
 
@@ -23,23 +22,28 @@ function Table_of_contents({mdBlock,page}:Props) {
         }
     },[])
 
+    const scrollToSection = (targetId:string) => {
+        const element = document.getElementById(targetId);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
   return (
     <div id={mdBlock.blockId} className='w-full'>
         {headingList.map((heading,i)=>{
             return (
-                <div key={i} className='mt-0.5 w-full py-1 rounded-md cursor-pointer hover:bg-neutral-100'>
-                    <Link href={`#${heading.blockId}`}>
-                        {heading.type===1 && <p className='ml-0.5 text-neutral-500 underline'>
-                            {heading.parent.slice(2)}</p>}
-                        {heading.type===2 && <p className='ml-5 mt-1 text-neutral-500 underline'>
-                            {heading.parent.slice(3)}</p>}
-                        {heading.type===3 && <p className='ml-8 text-neutral-500 underline'>
-                            {heading.parent.slice(4)}</p>}
-                    </Link>
+                <div key={i} onClick={()=>scrollToSection(heading.blockId)} className='mt-0.5 w-full py-1 rounded-md cursor-pointer hover:bg-neutral-100'>
+                    {heading.type===1 && <p className='ml-0.5 text-neutral-500 underline'>
+                        {heading.parent.slice(2)}</p>}
+                    {heading.type===2 && <p className='ml-5 mt-1 text-neutral-500 underline'>
+                        {heading.parent.slice(3)}</p>}
+                    {heading.type===3 && <p className='ml-8 text-neutral-500 underline'>
+                        {heading.parent.slice(4)}</p>}
                 </div>)
         })}
     </div>
   )
 }
 
-export default Table_of_contents
+export default Table_of_contents;
