0ccfd373b895cbbcc20448e4aba35e4e197da021
sasasanono <ukakau@gmail.com>
2025-02-25 18:01:57 +0900

    子ページにアイコン追加

--- components/mdBlocks/child_page/child_page.tsx
+++ components/mdBlocks/child_page/child_page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { MdBlock } from 'notion-to-md/build/types';
 import React from 'react';
+import Image from 'next/image';
 
 type Props = {
   mdBlock: MdBlock;
@@ -26,8 +27,8 @@ export default function ChildPage(props: Props) {
 
   return (
       <Link href={newPath} id={mdBlock.blockId}>
-        <div className=' my-0.5 py-1 px-0.5 hover:bg-neutral-100 cursor-pointer text-neutral-500 underline'>
-        {title}
+        <div className=' my-0.5 py-1 px-0.5 hover:bg-neutral-100 cursor-pointer text-neutral-500 underline flex'>
+        <Image src={'/horizon-atlas/file_icon.svg'} alt={''} height={20} width={20} className='w-5 h-5 m-0 mr-0.5' />{title}
         </div>
       </Link>
   );
