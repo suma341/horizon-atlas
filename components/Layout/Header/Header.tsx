afe6ceedea0c4d5d35b9106bc464054c764190d8
sasasanono <ukakau@gmail.com>
2025-02-26 18:16:54 +0900

    ロゴからリンク削除

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import React from 'react'
 import HamburgerButton from './hamburgerButton./hamburgerButton';
 import UserIcon from './UserInfo/userIcon';
@@ -16,9 +15,9 @@ export default function Header({setOpenbar,roleData}:Props){
   return (
     <header className='w-full bg-white'>
         <div className=" text-white px-5 mb-0 flex justify-between items-center">
-            <Link href="/posts">
-                <Image src={'/horizon-atlas/logo2_.png'} alt={''} width={32} height={16} className='w-[7.5rem] h-auto absolute top-[-29%] left-0.5' />
-            </Link>
+            <div>
+                <Image src={'/horizon-atlas/logo_.png'} alt={''} width={32} height={16} className='w-[7.5rem] h-auto absolute top-[-29%] left-0.5' />
+            </div>
             <ul className="hidden md:flex items-center pt-2 text-sm duration-100">
                 <li className="mr-4">
                     <SearchIcon />
