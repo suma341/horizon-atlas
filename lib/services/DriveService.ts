679ff7d4648dc13a621e81df78a8ea3470043913
sasasanono <ukakau@gmail.com>
2025-03-01 21:03:04 +0900

    読み込みファイルに対応

--- lib/services/DriveService.ts
+++ lib/services/DriveService.ts
@@ -1,58 +1,93 @@
 import { getDriveFileByFileId } from "../Gateways/DriveGateway";
 
-interface BaseInfo{
-    '学籍番号': string;
-    '名前': string;
-    '学舎': string;
-}
-
-interface FletCurriculumProgress extends BaseInfo{
+interface CurriculumProgress{
     [key: string]: string;
-    "1.Fletのインストール": string,
-    '2.カウンターを作ってみよう': string,
-    'Fletの練習問題1': string,
-    'Fletの練習問題2': string,
-    'Fletの練習問題3': string,
-    '電卓の作成': string,
 }
 
-type data = {
-    name:string;
+// type data = {
+//     name:string;
+//     title:string;
+//     isAchieved:boolean;
+// }
+
+type MetaData={
     title:string;
-    isAchieved:boolean;
+    fileId:string;
+    curriculums:string;
+    userName:string;
+    notAchievedText:string;
+    published: string;
 }
 
-export async function getFletCarriculumProgress(userName:string){
-    const fletCarriculumProgress:FletCurriculumProgress[] = await getDriveFileByFileId("1-HHH1HNuNVkyYN8T81xvXfVKez3rEje6yXiHREE7i_0");
-    const keys = Object.keys(fletCarriculumProgress[0])
-    const curriculums =  keys.slice(4).filter((item)=>item !== "");
-    const data = fletCarriculumProgress.map((item)=>{
-      const achievedList:data[] = [];
-      for(const key of curriculums){
-        if(item[key] !== ""){
-          achievedList.push({
-            title:key,
-            isAchieved:true,
-            name:item["名前"]
-          })
-        }
-      }
-      return achievedList
-    })
-    const userProgress = data.filter((item)=>item.find((d)=>d.name===userName)).flat();
-    if(userProgress.length===0){
-        return curriculums.map((item)=>{
-            return {
-                title:item,
-                isAchieved:false
+async function getMetaData(){
+    const metaDataFileId = "12ke9QhpI0icBvvCfk6Yyn7wKOeclF4_6OGdheopu-V8";
+    const metaData:MetaData[] = (await getDriveFileByFileId(metaDataFileId)).filter((item: { published: string; })=>item.published==="TRUE");
+    return metaData;
+}
+
+export async function getCarriculumProgress(userName:string){
+    const metaData = await getMetaData();
+    const progressData = await Promise.all(metaData.map(async(data)=>{
+        const allProgress:CurriculumProgress[] = await getDriveFileByFileId(data.fileId);
+        const progressByUser = allProgress.filter((item)=>item[data.userName]===userName)
+        const curriculums = data.curriculums.split("/");
+        const progress = progressByUser.map((p)=>{
+            const result:{
+                title:string;
+                achieved:boolean;
+            }[] = [];
+            for(const key of curriculums){
+                if(p[key] !== data.notAchievedText && p[data.userName] === userName){
+                    result.push({title:key,achieved:true})
+                }else if(p[key] === data.notAchievedText && p[data.userName] === userName){
+                    result.push({title:key,achieved:false})
+                }
             }
-        })
-    }
-    return curriculums.map((item)=>{
-        const a = userProgress.find((i)=>i.title===item);
-        return {
-            title:item,
-            isAchieved: a?.isAchieved ?? false,
+            return result;
+        }).flat()
+        if(progress.length===0){
+            curriculums.map((item)=>{
+                progress.push({title:item,achieved:false})
+            })
         }
-    })
+        return {
+            title:data.title,
+            progress:progress}
+    }))
+    return progressData;
   }
+
+// export async function getFletCarriculumProgress(userName:string){
+//     const fletCarriculumProgress:FletCurriculumProgress[] = await getDriveFileByFileId("1-HHH1HNuNVkyYN8T81xvXfVKez3rEje6yXiHREE7i_0");
+//     const keys = Object.keys(fletCarriculumProgress[0])
+//     const curriculums =  keys.slice(4).filter((item)=>item !== "");
+//     const data = fletCarriculumProgress.map((item)=>{
+//       const achievedList:data[] = [];
+//       for(const key of curriculums){
+//         if(item[key] !== ""){
+//           achievedList.push({
+//             title:key,
+//             isAchieved:true,
+//             name:item["名前"]
+//           })
+//         }
+//       }
+//       return achievedList
+//     })
+//     const userProgress = data.filter((item)=>item.find((d)=>d.name===userName)).flat();
+//     if(userProgress.length===0){
+//         return curriculums.map((item)=>{
+//             return {
+//                 title:item,
+//                 isAchieved:false
+//             }
+//         })
+//     }
+//     return curriculums.map((item)=>{
+//         const a = userProgress.find((i)=>i.title===item);
+//         return {
+//             title:item,
+//             isAchieved: a?.isAchieved ?? false,
+//         }
+//     })
+//   }
--- pages/user/progress/index.tsx
+++ pages/user/progress/index.tsx
@@ -1,13 +1,16 @@
 import { Loader2, CheckCircle, XCircle } from "lucide-react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect, useState } from "react";
-import { getFletCarriculumProgress } from "@/lib/services/DriveService";
+import { getCarriculumProgress } from "@/lib/services/DriveService";
 import Layout from "@/components/Layout/Layout";
 import { HOME_NAV, PROGRESS_NAV } from "@/constants/pageNavs";
 
 interface Progress {
   title: string;
-  isAchieved: boolean;
+  progress: {
+    title:string;
+    achieved: boolean;
+  }[];
 }
 
 export default function Progress() {
@@ -19,7 +22,7 @@ export default function Progress() {
     async function setData() {
       if (user) {
         setLoading(true);
-        const progress = await getFletCarriculumProgress(user.name!.split("-")[0]);
+        const progress = await getCarriculumProgress(user.name!.split("-")[0]);
         setProgress(progress);
         setLoading(false);
       }
@@ -31,7 +34,7 @@ export default function Progress() {
     <Layout headerProps={{ pageNavs: [HOME_NAV, PROGRESS_NAV] }}>
       <div className="min-h-screen text-gray-900 diagonal-bg pt-20 px-5">
         <div className="text-center mb-10">
-          <h1 className="text-3xl font-bold text-blue-500 animate-fadeIn">
+          <h1 className="text-3xl font-bold text-neutral-500 animate-fadeIn">
             {user?.name!.split("-")[0]}さんの進捗度
           </h1>
           <div className="w-40 mx-auto border-b-4 border-blue-300 mt-2"></div>
@@ -39,13 +42,18 @@ export default function Progress() {
 
         {loading ? (
           <div className="flex flex-col items-center gap-2">
-            <Loader2 className="animate-spin text-blue-500" size={48} />
+            <Loader2 className="animate-spin text-neutral-400" size={48} />
             <p className="text-lg font-semibold text-gray-700">読み込み中...</p>
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-8">
-            {progress.map((item, index) => (
-              <CurriculumItem key={index} {...item} />
+            {progress.map((item, i) => (
+                <div key={i}>
+                    <h2 className="text-neutral-400">{item.title}</h2>
+                        {item.progress.map((p,j)=>(
+                            <CurriculumItem key={j} {...p} />
+                        ))}
+                </div>
             ))}
           </div>
         )}
@@ -54,12 +62,12 @@ export default function Progress() {
   );
 }
 
-const CurriculumItem = ({ title, isAchieved }: { title: string; isAchieved: boolean }) => {
+const CurriculumItem = ({ title, achieved }: { title: string; achieved: boolean }) => {
   return (
-    <div className={`p-4 rounded-2xl shadow-md transition-all ${isAchieved ? "bg-green-100 hover:bg-green-200" : "bg-gray-100 hover:bg-gray-200"}`}>
+    <div className={`p-4 mt-2 rounded-2xl shadow-md transition-all ${achieved ? "bg-green-100 hover:bg-green-200" : "bg-gray-100 hover:bg-gray-200"}`}>
       <div className="flex items-center gap-3">
-        {isAchieved ? <CheckCircle className="text-green-500" size={24} /> : <XCircle className="text-red-500" size={24} />}
-        <span className="text-lg font-semibold">{title}</span>
+        {achieved ? <CheckCircle className="text-green-500" size={24} /> : <XCircle className="text-red-500" size={24} />}
+        <span className="text-base font-semibold">{title}</span>
       </div>
     </div>
   );
