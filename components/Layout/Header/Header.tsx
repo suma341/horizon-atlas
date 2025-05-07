27da6f38d6641216a7e1acf48288f226f81f478e
sasasanono <ukakau@gmail.com>
2025-05-07 14:45:40 +0900

    ロゴにホームページリンク埋め込み

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -16,9 +16,9 @@ export default function Header({setOpenbar}:Props){
   return (
     <header className='w-full bg-white'>
         <div className=" text-white px-5 mb-0 flex justify-between items-center">
-            <div>
-                <Image src={'/horizon-atlas/logo_.png'} alt={''} width={32} height={16} className='w-[7.5rem] h-auto absolute top-[-29%] left-0.5' />
-            </div>
+            <Link href={"/posts"}>
+                <Image src={'/horizon-atlas/logo_.png'} alt={''} width={32} height={16} className='w-32 h-11 absolute top-[-8%] left-0.5' />
+            </Link>
             <ul className="hidden md:flex items-center pt-2 text-sm duration-100">
                 <li className="mr-4">
                     <Link href={'/user/setting'}>
