183d3ebf1fb23fdeae27d8c254c7d392640b34a6
sasasanono <ukakau@gmail.com>
2025-05-30 12:02:06 +0900

    ogp画像改良

--- ogpScript/main.js
+++ ogpScript/main.js
@@ -3,7 +3,7 @@ import path from "path"
 import puppeteer from "puppeteer"
 import { getAll } from "./gateways/currilumGateway.js"
 import { getChildPageIds, getTitleAndIcon } from "./gateways/pageDataGateway.js";
-import { fileURLToPath, pathToFileURL } from "url";
+import { fileURLToPath } from "url";
 
 const allCurriculums = await getAll();
 const rootPages = allCurriculums.map((data)=>{
@@ -17,6 +17,9 @@ const allPageId = [...rootPages,...allChildId];
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const file_icon = `https://ryukoku-horizon.github.io/horizon-atlas/pngwing.png`;
+// console.log(file_icon,fs.existsSync(file_icon));
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -47,23 +50,22 @@ const __dirname = path.dirname(__filename);
       }
     }
     const page = await browser.newPage();
-    const file_icon = "https://ryukoku-horizon.github.io/horizon-atlas/pngwing.png"
     // ここで画像にしたいHTMLをセット
     const html = `
       <html>
         <body style="width: 1203px; height: 630px;background: white;">
-          ${coverUrl!=="" ? `<img src=${coverUrl} style="position: absolute;top: 0px;left:0px; width: 101%;height: 330px;">` : ""}
+          ${coverUrl!=="" ? `<img src=${coverUrl} style="position: absolute;top: 0px;left:-10px; width: 101%;height: 330px;object-fit: cover;">` : ""}
           ${coverUrl!=="" ? `<div>
-            ${iconType==="" ? `<img src="${file_icon} style="width: 9rem; height: 9rem;position: absolute; top: 265px; left: 90px" >` : ""}
-            ${(iconType !== "emoji" && iconType!=="") ? `<img src=${iconUrl} style="width: 9rem; height: 9rem;position: absolute; top: 265px; left: 90px" >` : ""}
-            ${iconType === "emoji" ? `<p style="font-size: 7rem; position: absolute; top: 160px; left: 90px">${iconUrl}</p>` : ""}
-            <h2 style="font-size: 64px; font-style: bold;position: absolute; top: 370px; left:40px;">${title}</h2>
+            ${iconType==="" ? `<img src="${file_icon}" style="width: 9rem; height: 9rem;position: absolute; top: 255px; left: 90px" >` : ""}
+            ${(iconType !== "emoji" && iconType!=="") ? `<img src=${iconUrl} style="width: 9rem; height: 9rem;position: absolute; top: 255px; left: 90px" >` : ""}
+            ${iconType === "emoji" ? `<p style="font-size: 7rem; position: absolute; top: 150px; left: 90px">${iconUrl}</p>` : ""}
+            <h2 style="font-size: 64px; font-style: bold;position: absolute; top: 350px; left:40px;">${title}</h2>
           </div>` :
-          `<div>
-            ${iconType==="" ? `<img src="${file_icon} style="width: 9rem; height: 9rem;position: absolute; top: 135px; left: 90px" >` : ""}
-            ${(iconType !== "emoji" && iconType!=="") ? `<img src=${iconUrl} style="width: 9rem; height: 9rem;position: absolute; top: 135px; left: 90px" >` : ""}
-            ${iconType === "emoji" ? `<p style="font-size: 7rem; position: absolute; top: 30px; left: 90px">${iconUrl}</p>` : ""}
-            <h2 style="font-size: 64px; font-style: bold;position: absolute; top: 220px; left:40px;">${title}</h2>
+          `<div style="display: flex; align-items: center; justify-content: center; flex:1; grid-gap: 5px;">
+            ${iconType==="" ? `<img src="${file_icon}" style="width: 9rem; height: 9rem;" >` : ""}
+            ${(iconType !== "emoji" && iconType!=="") ? `<img src=${iconUrl} style="width: 9rem; height: 9rem;" >` : ""}
+            ${iconType === "emoji" ? `<p style="font-size: 7.5rem;">${iconUrl}</p>` : ""}
+            <h2 style="font-size: 64px; font-style: bold;">${title}</h2>
           </div>`}
         </body>
       </html>
