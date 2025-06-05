121b74383a2215681d183371d8f135e3dab34116
sasasanono <ukakau@gmail.com>
2025-06-05 10:16:43 +0900

    ワークフロー内に日本語フォントインストールを追加

--- scripts/generateOgp/generate.js
+++ scripts/generateOgp/generate.js
@@ -70,11 +70,3 @@ export const generateOgpForCategory=async(browser,__dirname)=>{
     console.log(`Generated ${filePath}`);
   }
 }
-// type Category = {
-//   title: string;
-//   categoryId: string;
-//   description: string;
-//   cover: string;
-//   iconUrl: string;
-//   iconType: string;
-// }
