b8646da556c8f847efdebafc53315c9e1b0ca958
sasasanono <ukakau@gmail.com>
2025-08-07 16:03:07 +0900

    generateOGPスクリプト修正

--- scripts/generateOgp/createHTML.js
+++ scripts/generateOgp/createHTML.js
@@ -9,7 +9,7 @@ export const createHTML=(coverUrl,iconType,iconUrl,title)=>{
             ${(iconType !== "emoji" && iconType!=="") ? `<img src=${iconUrl} style="width: 9rem; height: 9rem;position: absolute; top: 235px; left: 90px" >` : ""}
             ${iconType === "emoji" ? `<p style="font-size: 7rem; position: absolute; top: 130px; left: 90px">${iconUrl}</p>` : ""}
             <h2 style="font-size: 64px; font-style: bold;position: absolute; top: 320px; left:40px;">${title}</h2>
-            <p style="font-size:24px; position: absolute; top: 440px; left: 40px;color:rgb(158, 159, 159);">on HorizonAtlas</p>
+            <p style="font-size:28px; position: absolute; top: 440px; left: 40px;color:rgb(158, 159, 159);">on HorizonAtlas</p>
           </div>` :
           `
           <div  style="display: flex;flex-direction: column;align-items: center; justify-content: center; flex:1;">
@@ -19,7 +19,7 @@ export const createHTML=(coverUrl,iconType,iconUrl,title)=>{
               ${iconType === "emoji" ? `<p style="font-size: 7.5rem;">${iconUrl}</p>` : ""}
               <h2 style="font-size: 64px; font-style: bold;">${title}</h2>
             </div>
-            <p style="font-size:24px; color:rgb(158, 159, 159);position: absolute; bottom: 20px; right: 50px;">on HorizonAtlas</p>
+            <p style="font-size:28px; color:rgb(158, 159, 159);position: absolute; bottom: 20px; right: 50px;">on HorizonAtlas</p>
           <div>
             `}
         </body>
--- scripts/generateOgp/gateways/currilumGateway.js
+++ scripts/generateOgp/gateways/currilumGateway.js
@@ -4,13 +4,20 @@ const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 
 export const getAll=async()=>{
-    const res = await fetch(`${SUPABASE_URL}/functions/v1/get_curriculum`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${SUPABASE_ANON_KEY}`
-        },
-    });
-    const result = await res.json();
-    return result;
+    try{
+        const res = await fetch(`${SUPABASE_URL}/functions/v1/getCurriculumWithSelect`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${SUPABASE_ANON_KEY}`
+            },
+            body:JSON.stringify({
+                select: "*"
+            })
+        });
+        const result = await res.json();
+        return result;
+    }catch(e){
+        console.error("error",e)
+    }
 }
