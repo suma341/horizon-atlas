311076f6f8f57bc626f27cb8332855a735685ab9
sasasanono <ukakau@gmail.com>
2026-01-03 15:23:25 +0900

    add workflow_dispatch to run notion2atlas from horizon-atlas

---
+++ scripts/runNotion2Atlas.js
@@ -0,0 +1,44 @@
+async function notifyDiscord(message) {
+  const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+
+  if (!webhookUrl) {
+    console.error("DISCORD_WEBHOOK_URL not set");
+    return;
+  }
+
+  await fetch(webhookUrl, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: "GitHub Actions",
+      content: message,
+    }),
+  });
+}
+
+const token = process.env.NOTION2ATLAS_GH_PAT;
+
+const res = await fetch(
+  "https://api.github.com/repos/suma341/notion2atlas/actions/workflows/update_repo.yml/dispatches",
+  {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/vnd.github+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ ref: "main" }),
+  }
+);
+
+if (!res.ok) {
+  const text = await res.text();
+  await notifyDiscord(`
+      horizon-atlasからnotion2atlasのworkflow実行に失敗しました
+      期限切れの可能性があるので、確認しましょう
+      error: ${text}
+    `)
+  console.error(text)
+}
+
+console.log("workflow dispatched");
