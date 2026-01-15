6998d004c47857715765595e9eaf179ef28a8c22
sasasanono <ukakau@gmail.com>
2026-01-15 15:59:30 +0900

    a

--- components/head/staticHead.tsx
+++ components/head/staticHead.tsx
@@ -11,11 +11,7 @@ const StaticHead=()=>{
             <meta property="og:image" content={`https://ryukoku-horizon.github.io/horizon-atlas/app_image.png`} />
             <meta property="og:type" content="website" />
             <meta property='og:site_name' content="HorizonAtlas" />
-            <meta name='twitter:title' content="HorizonAtlas" />
-            <meta name='twitter:description' content="HorizonAtlasは、RyukokuHorizonの学習カリキュラムをまとめた部員専用のサービスです。" />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:card" content="https://ryukoku-horizon.github.io/horizon-atlas/app_image.png" />
-            <link rel="icon" href="https://ryukoku-horizon.github.io/horizon-atlas/favicon.ico" />
+            <meta name="twitter:card" content="summary_large_image" />            
         </Head>
     )
 }
